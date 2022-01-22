319970465c48e122066258c4e4bb176f7b675d0b
Dovile-D <dovile.d.git@gmail.com>
2022-01-22 22:49:46 +0200

    add an initial commit

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
+# requesting data from both pages:
 response1 = requests.get("https://www.imdb.com/search/title/?groups=top_100&sort=user_rating,desc")
 imdb_html = response1.text
 
 response2 = requests.get("https://www.imdb.com/search/title/?groups=top_100&sort=user_rating,desc&start=51&ref_=adv_nxt")
 imdb_html2 = response2.text
 
+# scrapping from separate pages
 soup1 = BeautifulSoup(imdb_html, "html.parser")
 soup2 = BeautifulSoup(imdb_html2, "html.parser")
 
-
-all_movies1 = soup1.find_all(name="h3", class_="lister-item-header")
+# create two lists from separate pages:
+all_movie1 = soup1.find_all(name="h3", class_="lister-item-header")
 all_movies2 = soup1.find_all(name="h3", class_="lister-item-header")
 
 # list of both pages:
