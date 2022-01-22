9235f168971200e9d3865052cd24c2614d9cd91e
Dovile-D <dovile.d.git@gmail.com>
2022-01-22 22:35:41 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+
+response1 = requests.get("https://www.imdb.com/search/title/?groups=top_100&sort=user_rating,desc")
+imdb_html = response1.text
+
+response2 = requests.get("https://www.imdb.com/search/title/?groups=top_100&sort=user_rating,desc&start=51&ref_=adv_nxt")
+imdb_html2 = response2.text
+
+soup1 = BeautifulSoup(imdb_html, "html.parser")
+soup2 = BeautifulSoup(imdb_html2, "html.parser")
+
+
+all_movies1 = soup1.find_all(name="h3", class_="lister-item-header")
+all_movies2 = soup1.find_all(name="h3", class_="lister-item-header")
+
+# list of both pages:
+all_movies = all_movies1 + all_movies2
+
+rank = 1
+for movie in all_movies:
+    m = movie.a.string
+    row = f"{rank} {m}\n"
+    # making text file with a list:
+    with open("top_100_movies.txt", mode="a") as file:
+        file.write(row)
+    rank += 1
